c3f155576f20308100061cbd78d43a230705541c
indokalpa <indokalpasaikia1@gmail.com>
2025-11-17 17:18:29 +0530

    redirect to login page if not authenticated

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
-- Install 'axios' to handle 'fetch' requests(more convinient way)
+# DevTinder-web
 
-- CORS ERROR : install cors in backend => add middleware with configurations: origin, and credentials: true
+- create a Vite + React application
+- Install Tailwind Css
+- Install Daisy UI
+- Add NavBar Component to App.jsx
+- Install react-router-dom
+- Create BrowswerRouter -> Routes -> Route=/Body -> RouteChildren inside of <outLet />
+- Create a Login Page
+- Install axios, to make fetch requests more convinient
+- CORS : install cors in backend => add as middleware, with configurations: origin, credentials: true
 
-- Whenever you're making API call, pass axios {withCredentials: true}, to ensure to send back the token in API calls.
+- whenever you're making API call, pass {withCredential: true}, to pass along cookie in each requestt
+
+- install react-redux + @reduxjs/toolkit
+- configure redux store, create slice, use redux dev-tools
+- NavBar should update as soon as user logs in
+- Refactor code to add constants file + create a components folder
+- You should not be able to access other routes without login
+- If token is not Present, redirect user to login page
+- Avoid calls, if userData is already present
+- Logout feature
+- Profile page
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,16 +1,47 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import NavBar from './NavBar'
-import Footer from './Footer'
+import React, { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import { BASE_URL } from "../utils/contants";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import axios from "axios";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector(store => store.user);
+
+  const fetchUser = async () => {
+    if(userData) return;
+
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(res.data));
+    } catch (err) {
+      if(err.status === 401){
+        navigate("/login");
+      }
+
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div>
-        < NavBar />
-        < Outlet />
-        < Footer />
-    </div>
-  )
-}
+      <NavBar />
 
-export default Body
+      <Outlet />
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Body;
