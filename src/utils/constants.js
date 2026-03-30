8912491dc9c95b49ea9b3328046073a1fb25c792
indokalpa <indokalpasaikia1@gmail.com>
2026-03-30 22:56:36 +0530

    boom1

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,5 +2,7 @@
 // export const BASE_URL = "/api";
 
 // dev
-export const BASE_URL =
-    location.hostname === "localhost" ? "http://localhost:7777" : "/api";
+// export const BASE_URL =
+//     location.hostname === "localhost" ? "http://localhost:7777" : "/api";
+
+export const BASE_URL = "https://devtinder-45or.onrender.com"
